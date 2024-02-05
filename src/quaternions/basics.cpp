4a7720a385844c6500af05d5a1894ac16dbc7efe
ipmagamb <amir.ipmagambetov.contractor@draeger.com>
2024-02-05 14:56:29 +0200

    Quaternion rotation code

--- src/quaternions/basics.cpp
+++ src/quaternions/basics.cpp
@@ -1,48 +1,113 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <Framework.h>
 #include <Utils.h>
+#include <Shader.h>
+#include <OpenGlObjects.h>
+#include <CameraController.h>
+
 #include <glm/gtc/quaternion.hpp>
 #include <glm/gtx/quaternion.hpp>
+
+#include <imgui.h>
+#include <imgui_internal.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 #define S_WIDTH 1280
 #define S_HEIGHT 720
 
+void WindowSizeCallback(GLFWwindow* window, int width, int height) {
+    CameraController* cam = static_cast<CameraController*>(glfwGetWindowUserPointer(window));
+    cam->SetRatio((float)width / (float)height);
+}
+
+// Cube
+constexpr float vertices[] = {
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f
+};
+
+constexpr size_t NumVertices = sizeof(vertices) / sizeof(float) / 3;
+
+constexpr unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3
+};
 
 int main() {
     auto* window = Framework::CreateWindow(S_WIDTH, S_HEIGHT, "Quaternoins Basics");
+    Framework::EnableDepthTest();
 
-    const glm::vec3 axis_x(1.0f, 0.0f, 0.0f);
-    const float angle = 45.0f;
+    CameraController cam(S_WIDTH, S_HEIGHT);
+    glfwSetWindowUserPointer(window, &cam);
+    glfwSetWindowSizeCallback(window, WindowSizeCallback);
 
-    float angleRad = glm::radians(angle);
-    auto& axisNorm = glm::normalize(axis_x);
+    Shader shader(
+        res_dir + "/shaders/quat/vertex.glsl",
+        res_dir + "/shaders/quat/fragment.glsl"
+    );
 
-    float w = glm::cos(angleRad / 2);
-    float v = glm::sin(angleRad / 2);
-    glm::vec3 qv = axisNorm * v;
+    VertexArrayObject vao;
+    VertexBufferObject vbo;
+    ElementsBufferObject ebo;
+    vao.Bind();
+    vbo.Bind();
 
-    glm::quat quaternion(w, qv);
-    glm::mat4 quatTransform = glm::mat4_cast(quaternion);
-    
-    Utils::Print::Vec(qv);
-    Utils::Print::Vec(quaternion);
-    Utils::Print::Matrix(quatTransform);
+    vbo.SetData(sizeof(vertices), vertices);
+    ebo.Bind();
+    ebo.SetData(sizeof(indices), indices);
 
-    // Print::Vec(rotationQuat);
-    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-    // while (!glfwWindowShouldClose(window)) {
-    //     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-	// 	    glfwSetWindowShouldClose(window, true);
-    //     }
-        
-    //     glClear(GL_COLOR_BUFFER_BIT);
-    //     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    float phi = 45.0f;
+    const glm::vec3 u_q(1.0f, 0.0f, 0.0f);
 
+    glm::mat4 model(1.0);
 
-    // }
+    while (!glfwWindowShouldClose(window)) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		    glfwSetWindowShouldClose(window, true);
+        }
+
+        cam.Update();
+        cam.KeyboardCallback(window);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        Framework::ImGuiCallback([&] {
+            ImGui::Begin("Settings");
+            ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+            ImGui::Text("Quaternion Rotation");
+            ImGui::SliderFloat("Angle", &phi, -180.0f, 180.0f);
+
+            ImGui::End();
+        });
+
+        {
+            const auto phi_rad = glm::radians(phi);
+            glm::quat q_x(glm::cos(phi_rad), glm::sin(phi_rad * u_q)); // Rotates phi radians around u_q axis
+            model = glm::mat4_cast(q_x) * model;
+
+            shader.Use();
+            shader.SetMat4("projection", cam.GetPerspectiveMatrix());
+            shader.SetMat4("view", cam.GetViewMatrix());
+            shader.SetMat4("model", model);
+
+            vao.Bind();
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            vao.Unbind();
+        }
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
     Framework::Shutdown();
 
     return 0;
 }
+
